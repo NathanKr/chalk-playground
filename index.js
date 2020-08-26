9f636edff8a412cd3f32d011843552ce7eb55571
Nathan Krasney <natankrasney@gmail.com>
2020-08-26 12:00:52 +0300

    youtube version

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const chalk = require('chalk');
 const log = console.log;
 
-log(chalk.red('app is loading ... (red color)'));
+log(chalk.red('app is loading (color red)'));
 
-log(chalk.red.bold('app is loading ... (red color bold)'));
+log(chalk.green('app is loading (color green)'));
 
-log(chalk.bgBlue('app is loading ... (blue background)'));
+log(`${chalk.red('app is loading (color red)')} !!!! ${chalk.green('app is loading (color green)')}`);
 
-log(chalk.cyan.bgWhite('app is loading ... (cyan color) + (white background)'));
+log(chalk.rgb(255,165,0)('app is loading (color orange)'));
 
-log(chalk.rgb(255, 136, 0)('app is loading ... (orange color)'));
+log(chalk.rgb(255,165,0).bold('app is loading (color orange bold)'));
 
-log(chalk.rgb(255, 136, 0).bgRgb(165,42,42)('app is loading ... (orange color) + (brown background)'));
+log(chalk.bgBlue('app is loading (blue background)'));
 
+log(chalk.green.bgBlue('app is loading (blue background + green color)'));
+
+log(chalk.green.bgBlue.bold('app is loading (blue background + green color + bold)'));
